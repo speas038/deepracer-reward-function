9e30793d497f6b37b2c4ddf1a40f1b7bcde94b2f
Alex <alex.speasmaker@gmail.com>
2024-11-02 15:15:51 -0500

    About to figure out how to reward being closer to edge

--- reward.py
+++ reward.py
@@ -7,31 +7,19 @@ def reward_function(params):
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    steering_angle = params['steering_angle']
     all_wheels_on_track = params['all_wheels_on_track']
     progress = params['progress'] #Add an incentive eventually.  Higher rewards the closer you are to finishing
-    
     
     
     #incentivise being closer to the border when turning
     if all_wheels_on_track == False:
         print ("All wheels off track_width")
         return 1e-3
-
-        
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
     
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1.0
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
+    
+    #for higher steering angle give higher reward
+    
     
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 20 
@@ -40,6 +28,7 @@ def reward_function(params):
     if abs_steering > ABS_STEERING_THRESHOLD:
         reward *= 0.8
 
-    print ("returning reward: %reward %abs_steering")
+    print (params)
+    print("reward: {}".format(reward))
     
     return float(reward)
