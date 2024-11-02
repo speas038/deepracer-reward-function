57ed01d58b857c1044c8f09b80375cf5b1139d9e
Alex <alex.speasmaker@gmail.com>
2024-11-02 16:59:33 -0500

    broken boo

--- reward.py
+++ reward.py
@@ -9,7 +9,8 @@ def reward_function(params):
     abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
     steering_angle = params['steering_angle']
     all_wheels_on_track = params['all_wheels_on_track']
-    progress = params['progress'] #Add an incentive eventually.  Higher rewards the closer you are to finishing
+    progress = params['progress'] #Add an incentive eventually.  Higher rewards the close
+    is_left_of_center = params['is_left_of_center']
     
     
     #incentivise being closer to the border when turning
@@ -18,8 +19,28 @@ def reward_function(params):
         return 1e-3
     
     
-    #for higher steering angle give higher reward
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
     
+    
+    # Give higher reward if the car is closer to center line and vice versa if straight or higher turning angles means you should be closer to the track border[p-/.]
+    if distance_from_center <= marker_1 and 0 <= abs_steering <= 10:
+        reward = 1.0
+    elif distance_from_center <= marker_2 and 10 <= abs_steering <= 20:
+        reward = 1.0
+    elif distance_from_center <= marker_3 and 20 < abs_steering:
+        reward = 1.0
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+    if 5 <= steering_angle <= 30 and not is_left_of_center:
+        reward *= .8
+        
+    if -5 <= steering_angle <= -30 and is_left_of_center:
+        reward *= .8
+        
     
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 20 
