4734800c56abef3d145bbcee620ac29af1305e40
Alex <alex.speasmaker@gmail.com>
2024-11-06 08:40:31 -0600

    Everything normalized

--- reward.py
+++ reward.py
@@ -1,7 +1,7 @@
 def reward_function(params):
     
-    print("WELCOME")
-    print(params)
+    # print("WELCOME")
+    # print(params)
 
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
@@ -11,69 +11,75 @@ def reward_function(params):
     progress = params['progress'] #Add an incentive eventually.  Higher rewards the close
     is_left_of_center = params['is_left_of_center']
     speed = params['speed']
+
     
-    reward = normalize_speed(speed, abs_steering)
+    speed_reward = normalize_speed(speed, abs_steering)
+
+
     
     #incentivise being closer to the border when turning
     if all_wheels_on_track == False:
-        print ("All wheels off track_width")
+        # print ("All wheels off track_width")
         return 1e-3
     
-    '''   
-    # Calculate 3 markers that are at varying distances away from the center line
+
+    # Calculate 3 markers that are increasingly further away from the center line
     marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-    
-    
-    # Give higher reward if the car is closer to center line and vice versa if straight or higher turning angles means you should be closer to the track border[p-/.]
-    if distance_from_center <= marker_1 and 0 <= abs_steering <= 10:
-        reward = 1.0
-    elif distance_from_center <= marker_2 and 10 <= abs_steering <= 20:
-        reward = 1.0
-    elif distance_from_center <= marker_3 and 20 < abs_steering:
-        reward = 1.0
+    marker_2 = 0.5 * track_width
+    marker_3 = 0.9 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        location_reward = 1
+    elif distance_from_center <= marker_2:
+        location_reward = 0.7
+    elif distance_from_center <= marker_3:
+        location_reward = 0.5
     else:
-        reward = 1e-3  # likely crashed/ close to off track
-    '''
-    
+        location_reward = 1e-3  # likely crashed/ close to off track
+
+
     if 10 <= steering_angle <= 30 and not is_left_of_center:
-        reward *= .8
+        location_reward += .5
         
     if -10 <= steering_angle <= -30 and is_left_of_center:
-        reward *= .8
+        location_reward += .5
 
 
     
     # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 25
+    ABS_STEERING_THRESHOLD = 20
 
     # Penalize reward if the car is steering too much
     if abs_steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+        speed_reward *= 0.5
 
 #    print (params)
-    print("calculated unweighted reward: {}".format(reward))
 
     #Everything else accounts for .80% of reward, and progress the remaining 20%
 
-    weighted_progress_component = progress/100 * .20
-    weighted_calculated_reward = reward * .80
-    final_reward = weighted_calculated_reward + weighted_progress_component
+    weighted_progress_reward = progress/100 * .20
+    weighted_speed_reward = speed_reward * .30
+    weighted_location_reward = location_reward * .50
 
-    print("Weighted progress {} weighted reward {}".format(weighted_progress_component, weighted_calculated_reward))
+        
+    final_reward = weighted_speed_reward + weighted_progress_reward + weighted_location_reward
 
+    print("Weighted progress reward: {}".format(weighted_progress_reward))
+    print("Weighted speed_reward {}".format(weighted_speed_reward))
+    print("Weighted location reward: {}".format(weighted_location_reward))
+    print("Location reward: {}".format(location_reward))
     print("Final reward: {}".format(final_reward))
 
-    return float(weighted_progress_component + weighted_calculated_reward)
+    return float(final_reward)
 
+
+
+#Return a value between 0 and 1 for speed ( we will weight it later)    
 def normalize_speed(speed, abs_steering):
     #Lets say we are happy with a 1m/s slowdown per 10Degrees
-    ANGLE_ADJUST = .012 * abs_steering
-    normalized_speed = speed/5 + ANGLE_ADJUST
-    print("Normalize speed returning {}".format(normalized_speed) )
+    normalized_speed = speed/4
     return float(normalized_speed)
-    
 
 
 ##TESTS##
@@ -86,7 +92,7 @@ params = {'speed':5,
           'steering_angle':-25,
           'all_wheels_on_track':True,
           'progress': 100,
-          'is_left_of_center': True}
+          'is_left_of_center': False}
 
 
 reward_function(params)
